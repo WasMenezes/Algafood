84453105991b00720061315257a3a525ee591072
WasMenezes <was288519@gmail.com>
2022-01-18 19:35:11 -0300

    feature: add ResponseEntity

--- src/main/java/com/algaworks/algafood/api/controller/KitchenController.java
+++ src/main/java/com/algaworks/algafood/api/controller/KitchenController.java
@@ -5,6 +5,7 @@ import com.algaworks.algafood.domain.model.Kitchen;
 import com.algaworks.algafood.domain.repository.KitchenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,8 +26,12 @@ public class KitchenController {
     }
 
     @GetMapping("/{kitchenId}")
-    public Kitchen search(@PathVariable Long id) {
-        return kitchenRepository.byId(id);
+    public ResponseEntity<Kitchen> search(@PathVariable Long id) {
+        Kitchen kitchen =  kitchenRepository.byId(id);
+        if (kitchen != null) {
+            return ResponseEntity.ok(kitchen);
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
