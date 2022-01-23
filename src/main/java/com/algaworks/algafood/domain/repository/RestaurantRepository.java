67bf7a2da3bcce84ef0ca2c5177d2c7d9c7ed47f
WasMenezes <was288519@gmail.com>
2022-01-23 17:28:57 -0300

    feature: externalizing jpql in a xml file

--- src/main/java/com/algaworks/algafood/domain/repository/RestaurantRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestaurantRepository.java
@@ -2,7 +2,6 @@ package com.algaworks.algafood.domain.repository;
 
 import com.algaworks.algafood.domain.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -13,6 +12,5 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     List<Restaurant> findByShippingFeeBetween(BigDecimal initialFee, BigDecimal endFee);
 
-    @Query("from Restaurant where name like %:name% and kitchen.id = :kitchenId")
     Restaurant consultByName(String name, Long kitchenId);
 }
