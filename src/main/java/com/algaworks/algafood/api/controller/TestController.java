0d78b9074ab2422294a4c50d3fc6580de6d11c64
WasMenezes <was288519@gmail.com>
2022-01-23 15:21:13 -0300

    feature: create consult with query methods

--- src/main/java/com/algaworks/algafood/api/controller/TestController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.domain.model.Kitchen;
 import com.algaworks.algafood.domain.repository.KitchenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/teste")
@@ -13,8 +17,8 @@ public class TestController {
     @Autowired
     private KitchenRepository kitchenRepository;
 
-//    @GetMapping("kitchens/by-name")
-//    public List<Kitchen> kitchensByName(@RequestParam String name) {
-//        //return kitchenRepository.findBy(name);
-//    }
+    @GetMapping("kitchens/by-name")
+    public List<Kitchen> kitchensByName(@RequestParam String name) {
+        return kitchenRepository.findByName(name);
+    }
 }
