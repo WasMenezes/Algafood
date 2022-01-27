48497e92362d98bf3cf70b3d0419853f11406f4d
WasMenezes <was288519@gmail.com>
2022-01-27 18:55:02 -0300

    where with criteria api

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestaurantRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestaurantRepositoryImpl.java
@@ -3,12 +3,16 @@ package com.algaworks.algafood.infrastructure.repository;
 import com.algaworks.algafood.domain.model.Restaurant;
 import com.algaworks.algafood.domain.repository.RestaurantRepositoryQueries;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -25,7 +29,22 @@ public class RestaurantRepositoryImpl implements RestaurantRepositoryQueries {
 
         CriteriaQuery<Restaurant> criteria = builder.createQuery(Restaurant.class);
 
-        criteria.from(Restaurant.class);
+        Root<Restaurant> root = criteria.from(Restaurant.class);
+
+        var predicates = new ArrayList<Predicate>();
+        if (StringUtils.hasText(name)) {
+            predicates.add(builder.like(root.get("name"), "%" + name + "%"));
+        }
+
+        if (shippingFeeInitial != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("shippingFee"), shippingFeeInitial));
+        }
+
+        if (shippingFeeEnd != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("shippingFee"), shippingFeeEnd));
+        }
+
+        criteria.where(predicates.toArray(new Predicate[0]));
 
         return manager.createQuery(criteria)
                 .getResultList();
