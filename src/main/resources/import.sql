015c761647829ba2d8d5229cd4bb3030068c7de7
WasMenezes <was288519@gmail.com>
2022-01-28 23:58:48 -0300

    ManyToMany annotation

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -11,3 +11,9 @@ insert into state(id, name) values(2, 'Minas Gerais');
 insert into city(name, state_id) values('Colatina', 1);
 insert into city(name, state_id) values('Vitoria', 1);
 insert into city(name, state_id) values('Belo Horizonte', 2);
+
+insert into payment_method(id, description) values(1, 'Credit card');
+insert into payment_method(id, description) values(2, 'Debit card');
+insert into payment_method(id, description) values(3, 'Money');
+
+insert into restaurant_payment_method (restaurant_id, payment_method_id) values(1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
