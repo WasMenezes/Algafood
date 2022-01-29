5c0259cd73aad8331de601b707b091ec118ba73e
WasMenezes <was288519@gmail.com>
2022-01-28 23:34:06 -0300

    manyToOne annotation

--- src/main/java/com/algaworks/algafood/domain/model/Kitchen.java
+++ src/main/java/com/algaworks/algafood/domain/model/Kitchen.java
@@ -1,9 +1,12 @@
 package com.algaworks.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -16,4 +19,8 @@ public class Kitchen {
 
     @Column(nullable = false)
     private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "kitchen")
+    private List<Restaurant> restaurants = new ArrayList<>();
 }
